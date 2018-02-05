3e0fa3d2af1f171023bf4932601bbc7b464ef2ae
Carlos Kazuo <ninetails.nine@gmail.com>
2018-02-05 15:22:27 -0200

    Changed src/App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+const Page = ({ title }) => (
+  <div className="App">
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>{title}</h2>
+    </div>
+    <p className="App-intro">
+      This is the {title} page.
+      </p>
+    <p>
+      <Link to="/">Home</Link>
+    </p>
+    <p>
+      <Link to="/about">About</Link>
+    </p>
+    <p>
+      <Link to="/settings">Settings</Link>
+    </p>
+  </div>
+);
+
+const Home = (props) => (
+  <Page title="Home" />
+);
+
+const About = (props) => (
+  <Page title="About" />
+);
+
+const Settings = (props) => (
+  <Page title="Settings" />
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Fragment>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/settings" component={Settings} />
+        </Fragment>
+      </Router>
     );
   }
 }
